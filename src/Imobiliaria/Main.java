d4f9f1b9080a092b686938b54c85a9a48a0e6e14
Carlos <josecarlos.gadelha10@gmail.com>
2021-12-16 23:06:02 -0300

    first commit atualizado

--- src/Imobiliaria/Main.java
+++ src/Imobiliaria/Main.java
@@ -10,6 +10,7 @@ public class Main {
 		
 		for(int i=0; i<5; i++) {
 			imoveis[i] = f.criarImovel("casa", 01, 1000).faturamento ;
+			imoveis[i] = f.criarImovel("lote", 01, 20000).faturamento ;
 			System.out.println(imoveis[i]);
 		}
 		
